6bd20378574304b919020dd74aaf259884c5ad20
WmeLuna <warriorallen11@gmail.com>
2023-02-14 16:14:44 -0500

    add initial code

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,21 @@
-import { Injector, Logger, webpack } from "replugged";
+import { Injector, Logger, common, themes } from "replugged";
 
 const inject = new Injector();
-const logger = Logger.plugin("PluginTemplate");
+const logger = Logger.plugin("JS-Snippets");
+
+function F9(e: { key: string }): void {
+  if (e.key === "F9") {
+    logger.log("Refreshed theme");
+    common.toast.toast("Refreshed Theme");
+    themes.reload("com.wmeluna.Lucord");
+  }
+}
 
 export async function start(): Promise<void> {
-  const typingMod = await webpack.waitForModule<{
-    startTyping: (channelId: string) => void;
-  }>(webpack.filters.byProps("startTyping"));
-  const getChannelMod = await webpack.waitForModule<{
-    getChannel: (id: string) => {
-      name: string;
-    };
-  }>(webpack.filters.byProps("getChannel"));
-
-  if (typingMod && getChannelMod) {
-    inject.instead(typingMod, "startTyping", ([channel]) => {
-      const channelObj = getChannelMod.getChannel(channel);
-      logger.log(`Typing prevented! Channel: #${channelObj?.name ?? "unknown"} (${channel}).`);
-    });
-  }
+  document.body.addEventListener("keyup", F9);
 }
 
 export function stop(): void {
   inject.uninjectAll();
+  document.body.removeEventListener("keyup", F9);
 }
